a25b094b872f56ed4dd9bcf43468f0dce69bc28e
Deo Valiandro. M <mdv16h.student.unhas.ac.id>
2019-07-17 17:43:01 +0800

    ubah

--- resources/views/restaurant/create.blade.php
+++ resources/views/restaurant/create.blade.php
@@ -1,14 +1,12 @@
 @extends('layouts.app')
 @section('content')
-        <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-5">
             <div class="card mt-5">
                 <div class="card-header text-center">
                     Tambah Data Restoran
                 </div>
                 <div class="card-body">
-                    <a href="/restaurant" class="btn btn-primary">Kembali</a>
-                    <br/>
-                    <br/>
                     
                     {{ Form::open(['action' => 'RestaurantController@store'])}}
                     
@@ -27,7 +25,7 @@
 
                         <div class="form-group">
                                 {{Form::label('alamat','Alamat')}}
-                                {{Form::textarea('address', '', ['class' => 'form-control', 'placeholder' => 'Alamat'])}}                  
+                                {{Form::textarea('address', '', ['class' => 'form-control', 'placeholder' => 'Alamat', 'rows' => '4'])}}                  
                             
                              @if($errors->has('address'))
                                 <div class="text-danger">
@@ -37,29 +35,34 @@
 
                         </div>
 
-                        <div class="form-group">
-                                {{Form::label('latitude','Latitude')}}
-                                {{Form::text('lat', '', ['class' => 'form-control', 'placeholder' => 'Latitude'])}}                  
-                            
-                             @if($errors->has('lat'))
-                                <div class="text-danger">
-                                    {{ $errors->first('lat')}}
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                        {{Form::label('latitude','Latitude')}}
+                                        {{Form::text('lat', '', ['class' => 'form-control', 'placeholder' => 'Latitude'])}}                  
+                                    
+                                    @if($errors->has('lat'))
+                                        <div class="text-danger">
+                                            {{ $errors->first('lat')}}
+                                        </div>
+                                    @endif
+
                                 </div>
-                            @endif
-
-                        </div>
-
-                        <div class="form-group">
-                            {{Form::label('longitude','Longitude')}}
-                            {{Form::text('long', '', ['class' => 'form-control', 'placeholder' => 'Longitude'])}}                  
+                            </div>
                             
-                            
-                             @if($errors->has('long'))
-                                <div class="text-danger">
-                                    {{ $errors->first('long')}}
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    {{Form::label('longitude','Longitude')}}
+                                    {{Form::text('long', '', ['class' => 'form-control', 'placeholder' => 'Longitude'])}}                  
+                                    
+                                    
+                                    @if($errors->has('long'))
+                                        <div class="text-danger">
+                                            {{ $errors->first('long')}}
+                                        </div>
+                                    @endif
                                 </div>
-                            @endif
-
+                            </div>
                         </div>
 
                         <div class="form-group">
@@ -73,14 +76,18 @@
                             @endif
 
                         </div>
+                        
+                        <div id="mapid"></div>
 
                         <div class="form-group">
-                            {!! Form::submit('Simpan', ['class' => 'btn btn-primary']) !!}
+                            {!! Form::submit('Simpan', ['class' => 'btn btn-success']) !!}
+                            <a href="/restaurant" class="btn btn-light">Cancel</a>
                         </div>
 
                     {{ Form::close() }}
-
                 </div>
             </div>
         </div>
+    </div>
+</div>
 @endsection
