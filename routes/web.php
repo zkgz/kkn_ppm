a32ccbcc15a82f3242152670f1e44317909b51f0
Gibran Zidane <zidanezkg16h@student.unhas.ac.id>
2019-07-17 14:01:12 +0800

    added title to welcome view

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome')->with('title', 'KKN PPM Unhas');
 });
 
 Route::resource('restaurant', 'RestaurantController');
