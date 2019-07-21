34f3f6e77b62614076c4527894bf518cec201d67
Deo Valiandro. M <mdv16h.student.unhas.ac.id>
2019-07-21 21:39:34 +0800

    add images to create

--- resources/views/taxpayer/show.blade.php
+++ resources/views/taxpayer/show.blade.php
@@ -15,6 +15,7 @@
                             <tr><td>Latitude</td><td>{{ $taxpayer->lat }}</td></tr>
                             <tr><td>Longitude</td><td>{{ $taxpayer->long }}</td></tr>
                             <tr><td>Informations</td><td>{!! $taxpayer->information !!}</td></tr>
+                            <tr><td>Photo</td><td><img class="rounded mx-auto d-block" width="350px" src="{!! url('/data_file/'.$taxpayer->photo) !!}"></td></tr>
                         </tbody>
                     </table>
                 </div>
