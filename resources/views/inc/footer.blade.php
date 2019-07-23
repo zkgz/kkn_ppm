feab00addd431b03d3797940ff023e6417a714b2
Deo Valiandro. M <mdv16h.student.unhas.ac.id>
2019-07-23 20:32:15 +0800

    fix footer

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,22 +1,4 @@
     </div>
-
-    <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
-        <div class="container text-center">
-            <small>Copyright &copy; Team KKN PPM Program Pemetaan Spasial Pendapatan Daerah Kota Parepare</small>
-        </div>
-    </footer>
-
-    <style>
-        #sticky-footer{
-            position: absolute;
-            right: 0;
-            bottom: 0;
-            left: 0;
-            padding: 1rem;
-            background-color: #efefef;
-            text-align: center;
-        }
-    </style>
     
     <!-- jQuery first, then Popper, then Bootstrap -->
     <script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
@@ -29,7 +11,7 @@
 
     <!-- Script for datatables -->
     <script>
-        $(document).ready(function() {
+        $(document).ready(function(){
             var table = $('#table').DataTable();
             $('#table tfoot th').each( function () {
                 var title = $(this).text();
@@ -49,6 +31,17 @@
             });
         });
     </script>
+
+    <footer class="py-4 bg-dark text-white-50">
+        <div class="text-center">
+            <small>Copyright &copy; Team KKN PPM Program Pemetaan Spasial Pendapatan Daerah Kota Parepare</small>
+        </div>
+    </footer>
+
+    <style>
+        main{
+            min-height: 480px;
+        }
+    </style>
 </body>
 </html>
-
